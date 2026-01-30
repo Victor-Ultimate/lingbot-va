5ed0eb32046b34fe5c14f929d81e87ab6ebe02ef
zqh0253 <3170104343@zju.edu.cn>
2026-01-30 16:40:41 +0800

    init

---
+++ wan_va/configs/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2024-2025 The Robbyant Team Authors. All rights reserved.
+from .va_franka_cfg import va_franka_cfg
+from .va_robotwin_cfg import va_robotwin_cfg
+from .va_franka_i2va import va_franka_i2va_cfg
+from .va_robotwin_i2va import va_robotwin_i2va_cfg
+
+VA_CONFIGS = {
+    'robotwin': va_robotwin_cfg,
+    'franka': va_franka_cfg,
+    'robotwin_i2av': va_robotwin_i2va_cfg,
+    'franka_i2av': va_franka_i2va_cfg,
+}
